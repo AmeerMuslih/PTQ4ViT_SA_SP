616799f19044556c355470a7487bba0e13ddbbc6
Ameer Muslih <t-amuslih@microsoft.com>
2024-05-20 18:57:10 +0300

    fixing the following error: TypeError: unsupported operand type(s) for @: 'numpy.ndarray' and 'Tensor'

--- quant_layers/matmul.py
+++ quant_layers/matmul.py
@@ -32,7 +32,8 @@ class MinMaxQuantMatMul(nn.Module):
                 A_extended[i*Z: i*Z+Z, i*W:i*W+W] = A_3d[i, :, :]
 
             B_extended = B_3d.reshape(X*Y * W, Z)
-            result_2d = A_extended @ B_extended
+            A_extended_tensor = torch.from_numpy(A_extended)  # Convert numpy array to PyTorch tensor
+            result_2d = A_extended_tensor @ B_extended
             result_3d = result_2d.reshape(X*Y, Z, Z)
             # Reshape result back into the original shape
             out = result_3d.reshape(X, Y, Z, Z)
