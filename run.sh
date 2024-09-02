5dc8b5a8ba035e23d7f305c2041afb78f7fb3b35
ameermuslih <ameermuslih@microsoft.com>
2024-09-02 16:17:09 +0300

    fix run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 step=2
 
-for ((i=0; i<50; i+=step))
+for ((i=0; i<100; i+=step))
 do
-    nohup srun python3 ./my_test.py $((i * step)) $step & disown
+    nohup srun python3 ./my_test.py $i $step > ./output_$i & disown
 done
