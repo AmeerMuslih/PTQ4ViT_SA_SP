b9e7d325f95d83c8bc51fcdeb6c829077a8f55d6
ameermuslih <ameermuslih@microsoft.com>
2024-09-04 16:49:45 +0300

    update decimal points for SP

--- Aggregate_results.py
+++ Aggregate_results.py
@@ -13,16 +13,16 @@ def csvFilesMaker(all_util, Accumulator_TOT, InputA_TOT, InputB_TOT, totalCycles
 		Destination_path = "/home/firasramadan/miniconda3/Ameer_Project_Transformers/PTQ4ViT_SA_SP/OutputFiles/SP/" #"/home/a.mosa/Ameer/PTQ4ViT_Firas/OutputFiles/SP/"
 		os.makedirs('/home/firasramadan/miniconda3/Ameer_Project_Transformers/PTQ4ViT_SA_SP/OutputFiles/SP', exist_ok=True)
 		#os.makedirs('/home/a.mosa/Ameer/PTQ4ViT_Firas/OutputFiles/SP', exist_ok=True)
-		all_util_df = pd.DataFrame(all_util[:,:,0].numpy())
+		all_util_df = pd.DataFrame(all_util[:,:,0].numpy().round(3))
 		all_util_df.to_csv(Destination_path + filenamek_csv,header = False, index = False)
     
 		for bits in range(1,33): # Making files for each bit separately
 			if bits < 9:
 				filename_csv_InA = "InputA-SP-Bit"+str(9-bits)+"in"+str(dim)+"X"+str(dim)+"Dim.csv" #Bit 32 is the signbit
-				InA_df = pd.DataFrame(InputA_TOT[:,:,bits-1].numpy())
+				InA_df = pd.DataFrame(InputA_TOT[:,:,bits-1].numpy().round(3))
 				InA_df.to_csv(Destination_path + filename_csv_InA,header = False, index = False)
 				filename_csv_InB = "InputB-SP-Bit"+str(9-bits)+"in"+str(dim)+"X"+str(dim)+"Dim.csv" #Bit 32 is the signbit
-				InB_df = pd.DataFrame(InputB_TOT[:,:,bits-1].numpy())
+				InB_df = pd.DataFrame(InputB_TOT[:,:,bits-1].numpy().round(3))
 				InB_df.to_csv(Destination_path + filename_csv_InB,header = False, index = False)
 				#filename_csv_InA_toggle = "InputA-TR-Bit"+str(9-bits)+"in"+str(dim)+"X"+str(dim)+"Dim.csv" #Bit 32 is the signbit
 				#InA_toggle_df = pd.DataFrame(ToggleCount_InputA_Bits[:,:,bits-1,0].numpy())
@@ -32,7 +32,7 @@ def csvFilesMaker(all_util, Accumulator_TOT, InputA_TOT, InputB_TOT, totalCycles
 				#InB_toggle_df.to_csv(Destination_path + filename_csv_InB_toggle,header = False, index = False)
 			if bits < 17:	
 				filename_csv_mult = "Multiplier-SP-Bit"+str(17-bits)+"in"+str(dim)+"X"+str(dim)+"Dim.csv" #Bit 16 is the MSB
-				mult_df = pd.DataFrame(all_util[:,:,bits].numpy())
+				mult_df = pd.DataFrame(all_util[:,:,bits].numpy().round(3))
 				mult_df.to_csv(Destination_path + filename_csv_mult,header = False, index = False)
 				#filename_csv_mult_toggle = "Multiplier-TR-Bit"+str(17-bits)+"in"+str(dim)+"X"+str(dim)+"Dim.csv" #Bit 16 is the MSB
 				#mult_toggle_df = pd.DataFrame(ToggleCount_MultiplierBits[:,:,bits-1,0].numpy())
@@ -41,7 +41,7 @@ def csvFilesMaker(all_util, Accumulator_TOT, InputA_TOT, InputB_TOT, totalCycles
 			#accum_toggle_df = pd.DataFrame(ToggleCount_Accumulator_Bits[:,:,bits-1,0].numpy())
 			#accum_toggle_df.to_csv(Destination_path + filename_csv_accum_toggle,header = False, index = False)
 			filename_csv_accum = "Accumulator-SP-Bit"+str(33-bits)+"in"+str(dim)+"X"+str(dim)+"Dim.csv" #Bit 32 is the signbit
-			accum_df = pd.DataFrame(Accumulator_TOT[:,:,bits-1].numpy())
+			accum_df = pd.DataFrame(Accumulator_TOT[:,:,bits-1].numpy().round(3))
 			accum_df.to_csv(Destination_path + filename_csv_accum,header = False, index = False)
 
 def load_checkpoint(file_path):
