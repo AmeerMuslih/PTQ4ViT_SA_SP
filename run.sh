e457e007e49d231f85422d989c6f003bd393342d
ameermuslih <ameermuslih@microsoft.com>
2024-09-10 01:12:20 +0300

    add bit_flips as arg

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 
 num_images=$1
 num_runs=$2
+$bit_flips=$3
 
 for ((i=0; i<num_runs; i+=1))
 do
-    nohup srun -w newton5 python3 ./my_test.py 0 $num_images > ./output_$i & disown
+    nohup srun -w newton5 python3 ./my_test.py 0 $num_images $bit_flips > ./output_$i & disown
 done
